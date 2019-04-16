a76d16f1eff2de729d61a00d5e72708c69a1cf8e
Arsalan Thange <arsalan@cdpindia.com>
2019-04-16 17:51:21 +0530

    Added option to register middlewares in Middleware Class

--- app/middlewares/Middleware.php
+++ app/middlewares/Middleware.php
@@ -4,5 +4,32 @@ namespace App\Middlewares;
 
 class Middleware
 {
-    
+    /**
+     * Holds registered middlewares for this application.
+     *
+     * @var array
+     */
+    public $middlewares = [];
+
+    /**
+     * Construct authentication check.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->registerMiddlewares();
+    }
+
+    /**
+     * Register all midldewares for the application.
+     *
+     * @return void
+     */
+    protected function registerMiddlewares()
+    {
+        $this->middlewares = [
+            'auth' => \App\Middlewares\Authenticate::class,
+        ];
+    }
 }
