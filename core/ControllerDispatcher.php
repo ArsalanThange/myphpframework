cef9e218dadb4f6ff2af466c1fc2c65c15016509
Arsalan Thange <arsalan@cdpindia.com>
2019-04-17 16:32:30 +0530

    Create ControllerDispatcher.php
    ControllerDispatcher class will execute the method for the given route and dispatch it.

---
+++ core/ControllerDispatcher.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Core;
+
+use Core\Request;
+
+class ControllerDispatcher
+{
+    /**
+     * Namespace for controllers.
+     *
+     * @var string
+     */
+    protected $namespace = 'App\Controllers\\';
+
+    /**
+     * Default controller for routing.
+     *
+     * @var string
+     */
+    protected $controller = 'HomeController';
+
+    /**
+     * Default method for routing.
+     *
+     * @var string
+     */
+    protected $method = 'index';
+
+    /**
+     * Construct validation of incoming Request Route.
+     *
+     * @param string $controller    Controller for the requested route
+     * @param string $method        Method for the requested route
+     * @return void
+     */
+    public function __construct($controller, $method)
+    {
+        $this->controller = $controller;
+        $this->method = $method;
+
+        $this->dispatch();
+    }
+
+    /**
+     * Validate the requested route against registered routes.
+     *
+     * @return array
+     */
+    public function dispatch()
+    {
+        $controller = $this->namespace . $this->controller;
+        $c = new $controller;
+        $method = $this->method;
+
+        $request = new Request($_REQUEST);
+
+        $c->$method($request);
+    }
+}
