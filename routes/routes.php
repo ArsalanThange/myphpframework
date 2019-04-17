2739a13f1f0a96d7e76f6935c21b75f9394fea10
Arsalan Thange <arsalan@cdpindia.com>
2019-04-17 18:13:55 +0530

    Updated route registration using updated Route methods

--- routes/routes.php
+++ routes/routes.php
@@ -4,14 +4,14 @@ use Core\Route;
 
 $route = new Route;
 
-$route->get('/', 'HomeController@index', 'auth');
-$route->get('/login', 'LoginController@showlogin', 'guest');
-$route->post('/login', 'LoginController@login', 'guest');
+$route->get('/', 'HomeController@index')->middleware('auth');
+$route->get('/login', 'LoginController@showlogin')->middleware('guest');
+$route->post('/login', 'LoginController@login')->middleware('guest');
 $route->get('/logout', 'LoginController@logout', 'auth');
 
 //Demo Codes
-$route->get('/select', 'HomeController@select', 'auth');
-$route->get('/select2', 'HomeController@select2', 'auth');
-$route->get('/insert', 'HomeController@insert', 'auth');
-$route->get('/update', 'HomeController@update', 'auth');
-$route->get('/requestValidation', 'HomeController@requestValidation', 'auth');
+$route->get('/select', 'HomeController@select')->middleware('auth');
+$route->get('/select2', 'HomeController@select2')->middleware('auth');
+$route->get('/insert', 'HomeController@insert')->middleware('auth');
+$route->get('/update', 'HomeController@update')->middleware('auth');
+$route->get('/requestValidation', 'HomeController@requestValidation')->middleware('auth');
